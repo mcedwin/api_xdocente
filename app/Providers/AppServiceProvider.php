bd5a65aedbca07f15786529ea367053a43b15df4
Edwin <mcedwin@gmail.com>
2026-07-15 19:16:52 -0500

    rr

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,19 +8,21 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
+  /**
+   * Register any application services.
+   */
+  public function register(): void
+  {
+    //
+  }
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        DB::statement("SET time_zone = '+00:00'");
+  /**
+   * Bootstrap any application services.
+   */
+  public function boot(): void
+  {
+    if (DB::connection()->getPdo()) {
+      DB::statement("SET time_zone = '+00:00'");
     }
+  }
 }
